c3b693f3ef528f4ee4c1946b2b922a7649421ee8
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2026-04-11 20:22:00 +0530

    User model created

---
+++ backend/src/models/User.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"]
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    profilePic: {
+        type: String,
+        default: ""
+    }
+}, {
+    timestamps: true
+});
+
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
