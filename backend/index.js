82e1c52c65338df0d1853956c7d33237348a3f80
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2026-04-11 18:29:01 +0530

    Database connection

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import app from "./app.js";
+import connectDB from './src/config/db.js';
+
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT , () => {
-    console.log(`Server is running on : http://localhost:${PORT}`);
-});
+const serverStart = async() => {
+    try {
+        await connectDB();
+
+        app.listen(PORT , () => {
+            console.log(`Server is running on : http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.log("Server failed to start:", error.message);
+    }
+}
+
+serverStart();
+
